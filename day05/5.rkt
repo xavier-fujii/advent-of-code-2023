e823fe46c56fc798156c35bb78a143b6a439a8eb
Xavier Fujii <nothingtoworry7@gmail.com>
2023-12-08 23:06:38 +0800

    day 05

--- day05/5.rkt
+++ day05/5.rkt
@@ -51,22 +51,70 @@
 (define locations-1 (map (lambda (seed) (get-value-by-maps seed maps)) seeds-part-one))
 (define part-one-answer (apply min locations-1))
 (display part-one-answer)
-; (display "\n")
+(display "\n")
 ; ------ part one ------
 
 ; ------ part two ------
+(define (get-source-ranges map-item)
+  (sort
+   (map
+    (lambda (map-rule) (cons (second map-rule) (+ (second map-rule) (- (third map-rule) 1))))
+    map-item)
+   (lambda (a b) (< (car a) (car b)))))
+
 (define seeds-ranges
-  (map
-   (lambda (idx)
-     (let ([start (list-ref seeds-part-one idx)]
-           [length (list-ref seeds-part-one (+ 1 idx))]
-           ) (cons start (+ start length))))
-   (range 0 (length seeds-part-one) 2)))
+  (sort
+   (map
+    (lambda (idx)
+      (let ([start (list-ref seeds-part-one idx)]
+            [length (list-ref seeds-part-one (+ 1 idx))]
+            )
+        (cons start (- (+ start length) 1))))
+    (range 0 (length seeds-part-one) 2)
+    )
+   (lambda (a b) (< (car a) (car b)))))
 
-(display  seeds-ranges)
+(define (divide-range-by-range tb-div-range rang)
+  (let ([tb-start (car tb-div-range)]
+        [tb-end (cdr tb-div-range)]
+        [start (car rang)]
+        [end (cdr rang)])
+    (cond
+      [(< tb-end start)  (list tb-div-range)]
+      [(> tb-start end)  (list tb-div-range)]
+      [(and (< tb-end end) (> tb-start start))  (list tb-div-range)]
+      [(and (> tb-end end) (< tb-start start))  (list
+                                                 (cons tb-start (- start 1))
+                                                 (cons start end)
+                                                 (cons (+ end 1) tb-end))]
+      [(and (< start tb-end) (> start tb-start)) (list (cons tb-start (- start 1)) (cons start  tb-end))]
+      [(and (< end tb-end) (> end tb-start)) (list (cons tb-start end) (cons (+ end 1) tb-end))]
+      [else (list tb-div-range)])))
+
+(define (divide-ranges-by-range tb-div-ranges rang)
+  (apply append (list) (map (lambda (r) (divide-range-by-range r rang)) tb-div-ranges)))
+
+(define (divide-ranges-by-ranges tb-div-ranges rangs)
+  (foldl (lambda (a result) (divide-ranges-by-range result a)) tb-div-ranges rangs))
+
+(define (get-mapped-ranges input-ranges map-item)
+  (sort (let ([new-vs
+               (divide-ranges-by-ranges input-ranges (get-source-ranges map-item))])
+          (map
+           (lambda (v) (cons
+                        (get-value-by-map (car v) map-item)
+                        (get-value-by-map (cdr v) map-item)))
+           new-vs))
+        (lambda (a b) (< (car a) (car b)))))
+
+(define final-results
+  (foldl
+   (lambda (map-item result)
+     (get-mapped-ranges result map-item))
+   seeds-ranges
+   maps))
+
+(define part-two-answer (car (first final-results)))
+(display part-two-answer)
 (display "\n")
-
-
-; (define part-two-answer (apply min locations-2))
-; (display part-two-answer)
 ; ------ part two ------
